b2a8abf938497bc15366c59a7624ef0a60b7737a
Alan Basilio <fraterinti@gmail.com>
2024-09-28 22:05:16 -0300

    adding the react main structure

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae
+      repudiandae nam et magnam sunt vero quasi numquam repellat deserunt
+      repellendus incidunt eveniet, ullam sint? Est placeat ipsum eaque!
+      Explicabo, tempore?
+    </div>
+  );
+}
